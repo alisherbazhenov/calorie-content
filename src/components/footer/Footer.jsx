e69e75226a5e00fa0e8381e2820c41eef12534d0
Alisher <alisher.bazhenov88@gmail.com>
2023-11-30 15:42:39 +0300

    merge

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,25 +5,28 @@ import { ReactComponent as Wishlist } from '../../icons/wishlist.svg'
 import { ReactComponent as Dislike } from '../../icons/dislike.svg'
 
 const Footer = () => {
+  const items = [
+    { id: 'home', icon: <Home />, href: '#', title: 'Ссылка на главную' },
+    {
+      id: 'wishlist',
+      icon: <Wishlist />,
+      href: '#',
+      title: 'Ссылка на страницу списка',
+    },
+    { id: 'Dislike', icon: <Dislike />, href: '#', title: 'Кнопка лайка' },
+  ]
+
+  const listItems = items.map((item) => (
+    <li key={item.id} className={styles.item}>
+      <a className={styles.link} href={item.href} title={item.title}>
+        {item.icon}
+      </a>
+    </li>
+  ))
+
   return (
     <footer className={styles.footer}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            <Home className={styles.icon} />
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            <Wishlist className={styles.icon} />
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a className={styles.link} href="#">
-            <Dislike className={styles.icon} />
-          </a>
-        </li>
-      </ul>
+      <ul className={styles.list}>{listItems}</ul>
     </footer>
   )
 }
