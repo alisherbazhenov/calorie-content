dc582e70fb9e5988e4bb4a303050c632e6c0d8ec
Alisher <alisher.bazhenov88@gmail.com>
2023-12-07 10:11:30 +0300

    salads

--- src/components/Baklazhan/Baklazhan.jsx
+++ src/components/Baklazhan/Baklazhan.jsx
@@ -1,31 +1,57 @@
 /* eslint-disable */
+import { NavLink } from 'react-router-dom'
 import styles from './Baklazhan.module.scss'
+import { items } from './consts'
 
 export const Baklazhan = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>Baklazhan</h1>
+    <ul className={styles.nav__list}>
+      <li className={styles.nav__item}>
+        <NavLink
+          to="/baklazhan"
+          className={({ isActive }) => (isActive ? 'active-link' : 'nav__link')}
+        >
+          Салаты
+        </NavLink>
+      </li>
+      <li className={styles.nav__item}>
+        <NavLink
+          to="/baklazhan"
+          className={({ isActive }) => (isActive ? 'active-link' : 'nav__link')}
+        >
+          Завтраки
+        </NavLink>
+      </li>
+      <li className={styles.nav__item}>
+        <NavLink
+          to="/baklazhan"
+          className={({ isActive }) => (isActive ? 'active-link' : 'nav__link')}
+        >
+          Холодные закуски
+        </NavLink>
+      </li>
+      <li className={styles.nav__item}>
+        <NavLink
+          to="/baklazhan"
+          className={({ isActive }) => (isActive ? 'active-link' : 'nav__link')}
+        >
+          Супы
+        </NavLink>
+      </li>
+    </ul>
     <ul className={styles.list}>
-      <li className={styles.item}>
-        <a className={styles.link} href="#" title="Блюдо 1">
-          <img src="./img/BEL_5879" alt="Паста болоньезе" />
-          <h3 className={styles.subtitle}>Паста болоньезе</h3>
-          <p className={styles.text}>Бла бла бла состав такой-то и такой-то</p>
-        </a>
-      </li>
-      <li className={styles.item}>
-        <a className={styles.link} href="#" title="Блюдо 2">
-          <img src="./img/BEL_5879" alt="Паста болоньезе" />
-          <h3 className={styles.subtitle}>Плов</h3>
-          <p className={styles.text}>Бла бла бла состав такой-то и такой-то</p>
-        </a>
-      </li>
-      <li className={styles.item}>
-        <a className={styles.link} href="#" title="Блюдо 3">
-          <img src="./img/BEL_5879" alt="Паста болоньезе" />
-          <h3 className={styles.subtitle}>Хачапури</h3>
-          <p className={styles.text}>Бла бла бла состав такой-то и такой-то</p>
-        </a>
-      </li>
+      {items.map((item) => (
+        <li key={item.id} className={styles.item}>
+          <NavLink className={styles.link} to={item.to}>
+            <img className={styles.image} src={item.img} alt={item.alt} />
+            <div className={styles.dish}>
+              <p className={styles.dish__name}>{item.name}</p>
+              <p className={styles.dish__calorie}>{item.kcal} Ккал</p>
+            </div>
+          </NavLink>
+        </li>
+      ))}
     </ul>
   </div>
 )
--- src/components/Diet/Diet.jsx
+++ src/components/Diet/Diet.jsx
@@ -2,7 +2,7 @@ import styles from './Diet.module.scss'
 
 export const Diet = () => (
   <div className={styles.container}>
-    <h1>Рацион</h1>
+    <h1 className={styles.title}>Рацион</h1>
     <div>Графики, расчеты</div>
     <div>Список блюд</div>
   </div>
